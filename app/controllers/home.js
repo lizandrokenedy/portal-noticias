0e0abf096d80c2775223c2b9b5739e0e068bae70
lizandro kenedy <lisantos@mpmt.mp.br>
2021-02-13 15:05:03 -0400

    listando últimas notícias

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,3 +1,8 @@
 module.exports.index = (application, req, res) => {
-    res.render("home/index")
+    const connection = application.config.dbConnection();
+    const noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+    noticiasModel.obtemUltimasNoticias(function (error, result) {
+        res.render("home/index", {noticias: result})
+    })
 }
--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -14,6 +14,10 @@ NoticiasDAO.prototype.salvar = function (noticia, callback) {
     this._connection.query('INSERT INTO noticias set ?', noticia, callback)
 }
 
+NoticiasDAO.prototype.obtemUltimasNoticias = function(callback) {
+    this._connection.query('SELECT * FROM noticias ORDER BY data_criacao DESC LIMIT 5', callback)
+}
+
 module.exports = () => {
     return NoticiasDAO;
 }
